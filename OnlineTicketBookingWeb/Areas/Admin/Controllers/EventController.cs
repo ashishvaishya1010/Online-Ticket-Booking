13211135907810a264fec946c10c5f407b7b9a64
Ashish Vaishya <ashish.vaishya@kelltontech.org>
2023-05-31 10:11:31 +0530

    frontend started

---
+++ OnlineTicketBookingWeb/Areas/Admin/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineTicketBookingWeb.Areas.Admin.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
